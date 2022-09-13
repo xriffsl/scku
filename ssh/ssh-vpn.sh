e024e92ed6715fa7bf4f6f909e2355a834b34dd5
envy26 <109599884+envy26@users.noreply.github.com>
2022-09-13 12:31:00 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -249,7 +249,7 @@ echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 cat> /etc/issue.net << END
 <font color="red"><b>============================</b></font><br> 
-<font color="white"><b>         Bagoes vpn Service        </b></font><br> 
+<font color="white"><b>         Envy vpn Service        </b></font><br> 
 <font color="red"><b>============================</b></font>
 END
 
